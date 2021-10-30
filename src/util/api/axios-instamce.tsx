4d8458f3e8b4e88b17b3eb3b6fee92fa73b9e43f
elhaw <abd.elsayed@hotmail.com>
2021-10-30 16:05:19 +0200

    added axsios base config

--- src/util/api/axios-instamce.tsx
+++ src/util/api/axios-instamce.tsx
@@ -1,5 +1,11 @@
 import axios from "axios";
-const baseUrl = process.env.BASE_URL;
+const baseUrl = process.env.REACT_APP_BASE_URL;
+
 export const GoogleBooksAxios = axios.create({
   baseURL: baseUrl,
+});
+
+GoogleBooksAxios.interceptors.request.use((config) => {
+  config.params = { key: process.env.REACT_APP_API_KEY };
+  return config;
 });
