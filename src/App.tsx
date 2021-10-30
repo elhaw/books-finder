450a0b93b825fb95ccb81df4d8ac3b209a1af5d6
elhaw <abd.elsayed@hotmail.com>
2021-10-30 13:14:27 +0200

    added css styles, remoced unsed html tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { FormatedText } from "./components";
+import { BooksListContainer } from "./components/containers";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <FormatedText
+          textSize="XL"
+          Tag="h2"
+          textColor="primary"
+          textTransform="uppercase"
+          textType="heading"
+          text="book finder"
+        />
+        <BooksListContainer />
+      </div>
     </div>
   );
 }
