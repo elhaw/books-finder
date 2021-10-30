78480af42760e11e6139fb1d7c8a171a6bd66744
elhaw <abd.elsayed@hotmail.com>
2021-10-30 16:04:50 +0200

    entry file to all compoennts

---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+export { default as BookInfo } from "./BookInfo";
+export { default as BooksList } from "./BooksList";
+export { default as SearchBar } from "./SearchBar";
+export { default as FormatedText } from "./FormatedText";
+export { default as BooksListContainer } from "./containers/BooksListContainer";
